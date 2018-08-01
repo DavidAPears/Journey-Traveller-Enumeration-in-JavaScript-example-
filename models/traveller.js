676710b2726100f617c85c960e9f53ab29c91fbb
David A Pears <davidapears@gmail.com>
2018-08-01 16:22:18 +0100

    Initial & Final commit of tests, all working

---
+++ models/traveller.js
@@ -0,0 +1,38 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+  return this.journeys.map(journey => journey.startLocation)
+};
+
+
+Traveller.prototype.getJourneyEndLocations = function () {
+return this.journeys.map(journey => journey.endLocation)
+};
+
+Traveller.prototype.getModesOfTransport = function () {
+return this.journeys.map(journey => journey.transport)
+};
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+  return this.journeys.filter(journey => journey.transport === transport);
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+  return this.journeys.filter(journey => journey.distance > minDistance);
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+  let journeyDistance = this.journeys.map(journey => journey.distance);
+  return journeyDistance.reduce((total, journey) => {
+    return total + journey;
+  });
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function (transport) {
+  return Array.from(new Set(this.getModesOfTransport()));
+};
+
+
+module.exports = Traveller;
